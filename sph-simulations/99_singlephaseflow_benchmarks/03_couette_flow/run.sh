e9f2c2fa8ff07c02b27fc75f0e03a09fed18ed03
David Krach <david.krach@posteo.de>
2026-03-02 10:31:10 +0100

    [Updated] Timesteps

--- sph-simulations/99_singlephaseflow_benchmarks/03_couette_flow/run.sh
+++ sph-simulations/99_singlephaseflow_benchmarks/03_couette_flow/run.sh
@@ -4,6 +4,6 @@ mpirun -np 1 ./create_input_geometries_couette_flow.py 50
 mpirun -np 1 ./create_input_geometries_couette_flow.py 100
 
 mpirun -np 8 ./run_couette_flow.py 20 couette_flow_20_28_17_vs_5e-05_init.gsd 20001
-mpirun -np 8 ./run_couette_flow.py 30 couette_flow_30_38_17_vs_3.3333333333333335e-05_init.gsd 20001
-mpirun -np 8 ./run_couette_flow.py 50 couette_flow_50_58_17_vs_2e-05_init.gsd 20001
-mpirun -np 8 ./run_couette_flow.py 100 couette_flow_100_108_17_vs_1e-05_init.gsd 20001
+mpirun -np 8 ./run_couette_flow.py 30 couette_flow_30_38_17_vs_3.3333333333333335e-05_init.gsd 25001
+mpirun -np 8 ./run_couette_flow.py 50 couette_flow_50_58_17_vs_2e-05_init.gsd 40001
+mpirun -np 8 ./run_couette_flow.py 100 couette_flow_100_108_17_vs_1e-05_init.gsd 80001
