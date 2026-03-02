d660a5754135adda65ff987c4a7006b93e26b6ac
David Krach <david.krach@posteo.de>
2026-03-02 11:41:24 +0100

    [Updated] Timestep number

--- sph-simulations/99_singlephaseflow_benchmarks/01_poiseuille/run_atlas.sh
+++ sph-simulations/99_singlephaseflow_benchmarks/01_poiseuille/run_atlas.sh
@@ -3,7 +3,7 @@ mpirun -np 1 ./create_input_geometries_tube.py 30
 mpirun -np 1 ./create_input_geometries_tube.py 50 
 
 
-mpirun -np 1 ./run_tube_TV.py 20 poiseuille_flow_20_32_32_vs_5e-05_init.gsd 100001
+mpirun -np 1 ./run_tube_TV.py 20 poiseuille_flow_20_32_32_vs_5e-05_init.gsd 10001
 mpirun -np 20 ./run_tube_TV.py 50 poiseuille_flow_50_62_62_vs_2e-05_init.gsd 30001
 
 mpirun -np 1 ./run_tube.py 20 poiseuille_flow_20_32_32_vs_5e-05_init.gsd 10001
