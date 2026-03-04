062b80df4181d39c862ba97ee938cc1e81fb1332
David Krach <david.krach@posteo.de>
2026-03-04 18:38:53 +0100

    [Fix] Wolfgang script

--- build_wolfgang.sh
+++ build_wolfgang.sh
@@ -8,7 +8,7 @@ cd build
 CC=/usr/local.nfs/software/openmpi/4.1.4_gcc-11.3_cuda-11.7/bin/mpicc CXX=/usr/local.nfs/software/openmpi/4.1.4_gcc-11.3_cuda-11.7/bin/mpicxx cmake .. 
 make 
 cd $GIT_SRC
-cd dependencies/gsd-sph/gsd-3.2.0/
+cd dependencies/gsd-sph/gsd-3.4.2/
 rm -rf build
 mkdir build
 cd build 
