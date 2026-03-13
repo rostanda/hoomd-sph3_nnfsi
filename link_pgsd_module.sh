135938390d197e6f0f1feac7edf1be54ab07bc14
David Krach <122738533+krachdd@users.noreply.github.com>
2026-03-13 17:33:39 +0100

    Update symbolic links for pgsd source files

--- link_pgsd_module.sh
+++ link_pgsd_module.sh
@@ -3,5 +3,5 @@
 git submodule foreach --recursive git checkout main 
 
 export GIT_SRC=$(pwd)
-ln -s ${GIT_SRC}/dependencies/pgsd-sph/pgsd-3.2.0/pgsd/pgsd.c ${GIT_SRC}/hoomd-blue/hoomd/extern/pgsd.c
-ln -s ${GIT_SRC}/dependencies/pgsd-sph/pgsd-3.2.0/pgsd/pgsd.h ${GIT_SRC}/hoomd-blue/hoomd/extern/pgsd.h
+ln -s ${GIT_SRC}/dependencies/pgsd-sph/pgsd/pgsd/pgsd.c ${GIT_SRC}/hoomd-blue/hoomd/extern/pgsd.c
+ln -s ${GIT_SRC}/dependencies/pgsd-sph/pgsd/pgsd/pgsd.h ${GIT_SRC}/hoomd-blue/hoomd/extern/pgsd.h
