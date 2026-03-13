13757dce17335cf54b422cc0cdddf89697069bbb
David Krach <122738533+krachdd@users.noreply.github.com>
2026-03-13 17:30:42 +0100

    Update PYTHONPATH for gsd and pgsd dependencies

--- addsphpath.sh
+++ addsphpath.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export GIT_SRC=$(pwd)
 export PYTHONPATH=$PYTHONPATH:${GIT_SRC}/hoomd-blue/build
-export PYTHONPATH=$PYTHONPATH:${GIT_SRC}/dependencies/gsd-sph/gsd-3.4.2/build
-export PYTHONPATH=$PYTHONPATH:${GIT_SRC}/dependencies/pgsd-sph/pgsd-3.2.0/build
+export PYTHONPATH=$PYTHONPATH:${GIT_SRC}/dependencies/gsd-sph/gsd/build
+export PYTHONPATH=$PYTHONPATH:${GIT_SRC}/dependencies/pgsd-sph/pgsd/build
 export PYTHONPATH=$PYTHONPATH:${GIT_SRC}/helper_modules/gsd2vtu
 export PYTHONPATH=$PYTHONPATH:${GIT_SRC}/helper_modules/
